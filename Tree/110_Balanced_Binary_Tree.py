9b83063294785460541cf0101c1090d15dc67e35
ziwoncheong <32968721+ziwoncheong@users.noreply.github.com>
2021-10-03 01:26:42 +0900

    Update 110_Balanced_Binary_Tree.py

--- Tree/110_Balanced_Binary_Tree.py
+++ Tree/110_Balanced_Binary_Tree.py
@@ -19,7 +19,7 @@ class Solution:
         # 높이 차이가 나는 경우 -1, 이외에는 높이에 따라 1 증가
         if left == -1 or right == -1 or abs(left - right) > 1:
             return -1
-        return max(left, right) + 1
+        return max(left, right) + 1 # 트리가 위로 올라갈때마다 1씩 
         
         
         
