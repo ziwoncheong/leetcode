87ed35029214d0b73da35988de6d4ed9959c90ff
ziwoncheong <32968721+ziwoncheong@users.noreply.github.com>
2021-10-03 20:52:50 +0900

    Create 993_Cousins_in_Binary_Tree.py

---
+++ Tree/993_Cousins_in_Binary_Tree.py
@@ -0,0 +1,15 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isCousins(self, root: TreeNode, x: int, y: int) -> bool:
+        def dfs(node, parent, depth, mod):
+            if node:
+                if node.val == mod:
+                    return depth, parent
+                return dfs(node.left, node, depth + 1, mod) or dfs(node.right, node, depth + 1, mod)
+        dx, px, dy, py = dfs(root, None, 0, x) + dfs(root, None, 0, y)
+        return dx == dy and px != py
