35946db5feebbdd4288716e1c4623018fd7eac59
ziwoncheong <32968721+ziwoncheong@users.noreply.github.com>
2021-10-03 18:12:57 +0900

    Create 965_Univalued_Binary_Tree.py

---
+++ Tree/965_Univalued_Binary_Tree.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isUnivalTree(self, root: Optional[TreeNode]) -> bool:
+        if root:
+            if root.left:
+                if root.val != root.left.val:
+                    return False
+                
+            if root.right:
+                if root.val != root.right.val:
+                    return False
+            
+        else:
+            return True
+        return self.isUnivalTree(root.left) and self.isUnivalTree(root.right)
+        
